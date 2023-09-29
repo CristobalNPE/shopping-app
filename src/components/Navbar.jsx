bbf186c413f0335d843843040569107ff1e09fef
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-09-29 00:26:37 -0300

    fix navbar bottom border to occupy full width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,52 +11,54 @@ import {
 
 const Navbar = ({ switchNightMode }) => {
   return (
-    <nav className="max-w-screen-2xl mx-auto border-b-2  flex justify-between px-5 h-20 items-center gap-10">
-      <div className="flex gap-5">
-        <div className="px-2">
-          <h1 className="text-3xl text-primary font-semibold">ShC</h1>
+    <header className="border-b-2 ">
+      <nav className="max-w-screen-2xl mx-auto  flex justify-between px-5 h-20 items-center gap-10">
+        <div className="flex gap-5">
+          <div className="px-2">
+            <h1 className="text-3xl text-primary font-semibold">ShC</h1>
+          </div>
+          <Button
+            as={Link}
+            href="#"
+            color="primary"
+            className="px-9"
+            startContent={<MdGridView className="text-2xl" />}
+          >
+            Productos
+          </Button>
         </div>
-        <Button
-          as={Link}
-          href="#"
-          color="primary"
-          className="px-9"
-          startContent={<MdGridView className="text-2xl" />}
-        >
-          Productos
-        </Button>
-      </div>
-      <div className=" w-1/2 min-w-[20rem] max-w-[35rem]">
-        <Input
-          color="default"
-          size="xl"
-          placeholder="Buscar productos"
-          endContent={<MdSearch className="text-2xl" />}
-        />
-      </div>
-      <div>
-        <Switch
-          onChange={switchNightMode}
-          defaultSelected
-          size="md"
-          color="default"
-          startContent={<MdSunny />}
-          endContent={<MdNightlight />}
-        />
-      </div>
-      <div className="flex gap-3">
-        <Tooltip color="primary" content={"Carrito de compras"}>
-          <Button color="primary" variant="light" isIconOnly as={Link}>
-            <MdShoppingCart className="text-2xl" />
-          </Button>
-        </Tooltip>
-        <Tooltip color="primary" content={"Favoritos"}>
-          <Button color="primary" variant="light" isIconOnly as={Link}>
-            <MdFavorite className="text-2xl" />
-          </Button>
-        </Tooltip>
-      </div>
-    </nav>
+        <div className=" w-1/2 min-w-[20rem] max-w-[35rem]">
+          <Input
+            color="default"
+            size="xl"
+            placeholder="Buscar productos"
+            endContent={<MdSearch className="text-2xl" />}
+          />
+        </div>
+        <div>
+          <Switch
+            onChange={switchNightMode}
+            defaultSelected
+            size="md"
+            color="default"
+            startContent={<MdSunny />}
+            endContent={<MdNightlight />}
+          />
+        </div>
+        <div className="flex gap-3">
+          <Tooltip color="primary" content={"Carrito de compras"}>
+            <Button color="primary" variant="light" isIconOnly as={Link}>
+              <MdShoppingCart className="text-2xl" />
+            </Button>
+          </Tooltip>
+          <Tooltip color="primary" content={"Favoritos"}>
+            <Button color="primary" variant="light" isIconOnly as={Link}>
+              <MdFavorite className="text-2xl" />
+            </Button>
+          </Tooltip>
+        </div>
+      </nav>
+    </header>
   );
 };
 
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,6 +1,8 @@
 const Page = ({ children }) => {
   return (
-    <main className="bg-background max-w-screen-2xl mx-auto pt-5">{children}</main>
+    <main className="bg-background max-w-screen-2xl mx-auto px-5 pt-5">
+      {children}
+    </main>
   );
 };
 
