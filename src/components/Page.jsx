6a7e2aba755064b6db2e2f2702f364de3fe66566
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-09-28 23:47:38 -0300

    fix padding on page element

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,6 +1,6 @@
 const Page = ({ children }) => {
   return (
-    <main className="bg-background max-w-screen-2xl mx-auto ">{children}</main>
+    <main className="bg-background max-w-screen-2xl mx-auto pt-5">{children}</main>
   );
 };
 
