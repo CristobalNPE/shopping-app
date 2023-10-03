875479842f2a3b646d0b6706690281062c7c38c5
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-03 18:26:33 -0300

    create component for reusability

---
+++ src/components/RedirectCard.jsx
@@ -0,0 +1,28 @@
+import { Button } from "@nextui-org/react";
+import { NavLink } from "react-router-dom";
+
+const RedirectCard = ({ title, subtitle, toLink, setIsOpen }) => {
+  if (setIsOpen === undefined) {
+    setIsOpen = () => {};
+  }
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-3 text-center">
+      <h1 className="text-2xl font-normal">{title}</h1>
+      <h2>{subtitle}</h2>
+      <Button
+        className="mt-12 w-full sm:w-fit"
+        color="success"
+        size="lg"
+        variant="shadow"
+        as={NavLink}
+        to={toLink}
+        onClick={() => setIsOpen(false)}
+      >
+        Ver Catálogo
+      </Button>
+    </div>
+  );
+};
+
+export default RedirectCard;
