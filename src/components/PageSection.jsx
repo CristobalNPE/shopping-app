c1e5cade65a4c96f04eccd423e4241c3a86a93ce
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-03 19:32:19 -0300

    delete pagesection, useless

--- src/components/PageSection.jsx
+++
@@ -1,9 +0,0 @@
-const PageSection = ({ children }) => {
-  return (
-    <section className=" h-screen sm:items-start text-center sm:text-left items-center">
-      {children}
-    </section>
-  );
-};
-
-export default PageSection;
