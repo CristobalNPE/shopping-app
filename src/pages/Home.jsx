fcec244dc218585cd00d2323795a2d96a51322e7
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-04 20:37:17 -0300

    add some animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,31 @@
 import { Button } from "@nextui-org/react";
 import Page from "../components/Page";
 import { Link } from "react-router-dom";
-
+import { motion } from "framer-motion";
 const Home = () => {
   return (
     <>
       <Page animated>
         <div className="mt-unit-4xl sm:mt-unit-5xl">
-          <h2 className="mb-4 text-center text-3xl font-thin  sm:text-left sm:text-5xl">
+          <motion.h2
+            initial={{ opacity: 0, y: -200 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="mb-4 text-center text-3xl font-thin  sm:text-left sm:text-5xl"
+          >
             Bienvenido a
-          </h2>
+          </motion.h2>
           <h1 className="text-center text-5xl font-semibold sm:text-left  sm:text-7xl md:text-8xl">
             Shopper&#39;s Choice
           </h1>
 
-          <h3 className="text-md  line mt-16 text-center font-light leading-8 sm:mt-5  sm:text-left sm:text-lg">
-            Empieza a{" "}<br className="sm:hidden"/>
+          <motion.h3
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="text-md  line mt-16 text-center font-light leading-8 sm:mt-5  sm:text-left sm:text-lg"
+          >
+            Empieza a <br className="sm:hidden" />
             <Button
               color="success"
               variant="flat"
@@ -25,8 +35,8 @@ const Home = () => {
             >
               explorar nuestros productos
             </Button>{" "}
-            <br className="sm:hidden"/>y  descubre la verdadera comodidad.
-          </h3>
+            <br className="sm:hidden" />y descubre la verdadera comodidad.
+          </motion.h3>
         </div>
       </Page>
     </>
