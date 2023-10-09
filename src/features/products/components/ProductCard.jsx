717755f1811761b6a181bb3814e33cb7a9183d67
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-09 15:38:36 -0300

    update cards to have favorite toggle

--- src/features/products/components/ProductCard.jsx
+++ src/features/products/components/ProductCard.jsx
@@ -6,8 +6,9 @@ import {
   Chip,
   Image,
 } from "@nextui-org/react";
-import { MdStar } from "react-icons/md";
+import { MdFavorite, MdFavoriteBorder, MdStar } from "react-icons/md";
 import { NavLink } from "react-router-dom";
+import useFavorites from "../../../hooks/useFavorites";
 
 const ProductCard = ({
   id,
@@ -19,6 +20,13 @@ const ProductCard = ({
   search,
   categoryFilter,
 }) => {
+  const { toggleFavorite, isFavorite } = useFavorites();
+
+  const handleFavClick = (e) => {
+    e.stopPropagation();
+    toggleFavorite(id);
+  };
+
   return (
     <Card
       as={NavLink}
@@ -27,9 +35,17 @@ const ProductCard = ({
       shadow="sm"
       className="max-w-[20rem] bg-background p-3 text-foreground drop-shadow-lg hover:shadow-inner"
     >
-      <CardHeader className="flex justify-end">
-        <span className="flex items-center gap-1">
-          <MdStar className="text-2xl text-primary" /> {rating.rate}
+      <CardHeader className="flex justify-between">
+        <Chip size="md" className="mt-2" color="secondary">
+          {category}
+        </Chip>
+
+        <span onMouseDown={(e) => handleFavClick(e)}>
+          {isFavorite(id) ? (
+            <MdFavorite className="text-3xl text-danger" />
+          ) : (
+            <MdFavoriteBorder className="text-3xl text-danger" />
+          )}
         </span>
       </CardHeader>
       <CardBody className="max-h-[15rem] p-0">
@@ -42,13 +58,17 @@ const ProductCard = ({
           />
         </div>
       </CardBody>
-      <CardFooter className="flex   h-1/2">
+      <CardFooter className="flex h-1/2">
         <div className="flex h-full  flex-col justify-between">
           <h1 className="font-semibold">{title}</h1>
-          <h2 className="text-2xl font-bold">$ {price}</h2>
-          <Chip className="mt-2" color="secondary">
-            {category}
-          </Chip>
+          <div className="flex flex-col gap-3">
+            <span className="text-md flex items-center gap-1 ">
+              <MdStar className="text-2xl text-primary" />{" "}
+              <span className="mr-1 font-semibold">{rating.rate}</span> (
+              {rating.count} reviews)
+            </span>
+            <h2 className="text-2xl font-bold">$ {price}</h2>
+          </div>
         </div>
       </CardFooter>
     </Card>
